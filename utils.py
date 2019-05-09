219d1873ed71538998a798ae78694fbec101c5ce
Kathrine Gibson <kgzinnia@gmail.com>
2019-05-09 11:22:54 -0700

    jupyter notebook complete

--- utils.py
+++ utils.py
@@ -1,6 +1,68 @@
 import random
+import matplotlib.pyplot as plt
 import math
 import numpy as np
+
+'''
+Converts string values to usable numeric values
+Parameter table: a single column table to be converted
+Returns return_table: a table of converted values
+'''
+
+
+def convert_to_numeric(table):
+    return_table = []
+    index = 0
+    for value in table:
+        return_table.append(float(table[index]))
+        index += 1
+    return return_table
+
+
+'''
+Finds the coefficient for the linear regression of a scatterplot
+Parameter xvalues: the table of values for the x axis
+Parameter yvalues: the table of values of the y axis
+Returns (b1,b2): the slope and y intercept of the linear regression
+'''
+
+
+def get_coefficient(xvalues, yvalues):
+    size = np.size(xvalues)
+    # find the mean of xvalues and yvalues
+    xmean = np.mean(xvalues)
+    ymean = np.mean(yvalues)
+    # calculate the cross-deviation and deviation about x
+    cross_deviation = np.sum(yvalues*xvalues) - size*ymean*xmean
+    xdeviation = np.sum(xvalues*xvalues) - size*xmean*xmean
+    # calculate the regression coefficients
+    b2 = cross_deviation / xdeviation  # this is the slope (m)
+    b1 = ymean - b2*xmean  # this is y intercept (b)
+    return (b1, b2)
+
+
+'''
+Creates a scatter plot comparing two data tables
+Parameter yaxis_column: single column table to be plotted on the y axis
+Parameter xaxis_column: single column table to be plotted on the x axis
+Parameter filename: the name of the file to save the graph to
+Parameter titlename: the title for the graph
+Parameter yaxis_label: the label for the y axis
+Parameter step: the step of the program to plot
+'''
+
+
+def create_scatter_plot(xaxis_column, yaxis_column, xlabel):
+    plt.figure()
+    xvalues = convert_to_numeric(xaxis_column)
+    yvalues = convert_to_numeric(yaxis_column)
+    plt.scatter(xvalues, yvalues, c="b")
+    plt.grid(True)
+    plt.xlabel(xlabel)
+    plt.ylabel("Popularity")
+    plt.title(xlabel + " vs Popularity")
+    plt.show()
+
 
 '''
 FROM CLASS
@@ -59,7 +121,7 @@ def read_file_to_table(filename, table, indices=None):
                     trimmed_values.append(float(values[i]))
             table.append(trimmed_values)
         else:
-            convert_to_numeric(values)
+            convert_to_numeric1(values)
             table.append(values)  # adds to the end
     infile.close()
 
@@ -71,7 +133,7 @@ def read_file_to_table(filename, table, indices=None):
     '''
 
 
-def convert_to_numeric(values):
+def convert_to_numeric1(values):
     for i in range(len(values)):
         try:
             numeric_val = float(values[i])
